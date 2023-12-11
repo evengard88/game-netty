6a61e9ff2d37c4e4a6e97f081910a7fa8b71a29c
Elijah <ikurenkov88@gmail.com>
2023-12-11 08:11:58 +0300

    remove dockerfile comment

--- src/main/java/me/ikurenkov/game/application/GameHandler.java
+++ src/main/java/me/ikurenkov/game/application/GameHandler.java
@@ -1,7 +1,5 @@
 package me.ikurenkov.game.application;
 
-import me.ikurenkov.game.application.GameContext;
-
 public interface GameHandler {
     void handle(GameContext context, String message);
     boolean supports(GameContext context);
