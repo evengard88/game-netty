e7758487baa4eb20cfdf4dcfde8ef5e9c64d0471
Elijah <ikurenkov88@gmail.com>
2023-12-12 18:21:45 +0300

    double check locking

--- src/main/java/me/ikurenkov/game/application/handler/GameStartHandler.java
+++ src/main/java/me/ikurenkov/game/application/handler/GameStartHandler.java
@@ -1,16 +1,19 @@
 package me.ikurenkov.game.application.handler;
 
 import com.google.inject.Inject;
+import lombok.SneakyThrows;
 import me.ikurenkov.game.application.GameContext;
 import me.ikurenkov.game.application.GameHandler;
 import me.ikurenkov.game.domain.Game;
 import me.ikurenkov.game.domain.Player;
-import lombok.SneakyThrows;
 
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class GameStartHandler implements GameHandler {
     private final BlockingQueue<GameContext> lobby;
+    private final static Lock lock = new ReentrantLock();
 
     @Inject
     public GameStartHandler(BlockingQueue<GameContext> lobby) {
@@ -25,9 +28,19 @@ public class GameStartHandler implements GameHandler {
             actor.sendMassage("Searching for opponent...");
             GameContext secondGameContext = lobby.poll();
             if (secondGameContext == null) {
-                lobby.add(context);
-                actor.sendMassage("No opponent available! Wait for opponent.");
-            } else {
+                try {
+                    lock.lock();
+                    secondGameContext = lobby.poll();
+                    if (secondGameContext == null) {
+                        lobby.add(context);
+                        actor.sendMassage("No opponent available! Wait for opponent.");
+                    }
+                } finally {
+                    lock.unlock();
+                }
+            }
+
+            if (secondGameContext != null) {
                 Player secondPlayer = secondGameContext.getActor();
                 Game game = new Game(actor, secondPlayer);
                 context.setGame(game);
